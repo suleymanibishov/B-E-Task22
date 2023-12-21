bab5a6b649cd5b5055909fac5c59eb633e389b8c
suleymanibishov <suleyman22022@gmail.com>
2023-12-21 17:13:12 +0400

    Add action

--- B-E-Task22/Areas/Admin/Controllers/HomeController.cs
+++ B-E-Task22/Areas/Admin/Controllers/HomeController.cs
@@ -17,6 +17,7 @@ namespace B_E_Task22.Areas.Admin.Models
             _db = db;
           
         }
+
         public IActionResult Index()
         {
             return View( _db.RecentWorkCompononents.ToList());
@@ -31,11 +32,59 @@ namespace B_E_Task22.Areas.Admin.Models
         public IActionResult Creat(RecentWorkCompononent recent)
         {
             if(!ModelState.IsValid) return View(recent);
+            if (_db.RecentWorkCompononents.Any(r => r.Title == recent.Title))
+            {
+                ModelState.AddModelError("Title", "Eyni adan istifade Olunub");
+                return View(recent);
+            }
             _db.RecentWorkCompononents.Add(recent);
             _db.SaveChanges();
             return RedirectToAction(nameof(Index));
         }
 
+        public async Task<IActionResult> Update(int id)
+        {
+            var model =await _db.RecentWorkCompononents.FindAsync(id);
+            if (model == null) return NotFound();
+            return View(model);
+        }
 
+        public async Task<IActionResult> Detail(int id)
+        {
+            var model =await _db.RecentWorkCompononents.FindAsync(id);
+            if (model == null) return NotFound();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(int id, RecentWorkCompononent recent)
+        {
+           
+            if (!ModelState.IsValid) return View(recent);
+            if(_db.RecentWorkCompononents.Any(r=>r.Title == recent.Title && r.Id != recent.Id))
+            {
+                ModelState.AddModelError("Title", "Eyni adan istifade Olunub");
+                return View(recent);
+            }
+            if (id != recent.Id) return BadRequest();
+            var dbRecent = await _db.RecentWorkCompononents.FindAsync(id);
+            if(dbRecent == null) return NotFound();
+            dbRecent.Title = recent.Title;
+            dbRecent.Description= recent.Description;
+            dbRecent.Image= recent.Image;
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var dbRecent = await _db.RecentWorkCompononents.FindAsync(id);
+            if (dbRecent == null) return NotFound();
+            _db.RecentWorkCompononents.Remove(dbRecent);
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
